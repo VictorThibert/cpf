81b6b786e1c5415eea5ee8ff19087a229194d72d
Ahmed Khan <khanaahmed10@gmail.com>
2018-01-20 23:36:48 -0500

    removed comments on googleapi

--- server/googleapi.py
+++ server/googleapi.py
@@ -20,42 +20,11 @@ def get_google_results(location='Montreal, Canada', radius=20000):
         for place in query_result.places:
             place.get_details()
             yield place
-        
+
         if query_result.has_next_page_token:
             query_result = google_places.nearby_search(
         	    pagetoken=query_result.next_page_token)
         else: break
-        """
-        # Returned places from a query are place summaries.
-        print place.name
-        print place.geo_location
-        print place.place_id
-
-        # The following method has to make a further API call.
-        place.get_details()
-        # Referencing any of the attributes below, prior to making a call to
-        # get_details() will raise a googleplaces.GooglePlacesAttributeError.
-        print place.details # A dict matching the JSON response from Google.
-        print place.local_phone_number
-        print place.international_phone_number
-        print place.website
-        print place.url
-
-        # Getting place photos
-
-        for photo in place.photos:
-            # 'maxheight' or 'maxwidth' is required
-            photo.get(maxheight=500, maxwidth=500)
-            # MIME-type, e.g. 'image/jpeg'
-            photo.mimetype
-            # Image URL
-            photo.url
-            # Original filename (optional)
-            photo.filename
-            # Raw image data
-            photo.data
-        """
-
     # Are there any additional pages of results?
     if query_result.has_next_page_token:
         query_result_next_page = google_places.nearby_search(
