f6309b259df508b09adc0d4177761e94c43534f8
Victor Thibert <victor.thibert@gmail.com>
2018-02-03 14:30:49 -0500

    get_list flask returns n restaurants

--- server/restaurants.py
+++ server/restaurants.py
@@ -8,12 +8,10 @@ from extensions import db
 
 restaurant = Blueprint('restaurant', __name__, url_prefix='/restaurant')
 
-logging.info("this is from the game file");
-
+# get restaurant information given a specific restaurant id (using MongoDB id)
 @restaurant.route('/<restaurant_id>/info')
-def get_rest_info(rest_id):
-    """ gets info for a given restaurant """
-    _id = ObjectId(rest_id)
+def get_restaurant_info(restaurant_id):
+    _id = ObjectId(restaurant_id)
     result = db.restaurants.find_one({
         '_id': _id
     },{
@@ -28,17 +26,41 @@ def get_rest_info(rest_id):
     result['success'] = True
     return jsonify(result)
 
+# get photos 
 @restaurant.route('/<restaurant_id>/photo/<photo_id>')
-def get_info(rest_id, photo_id):
-    result = db.restaurant.find_one({
-        'id':rest_id,
+def get_info(restaurant_id, photo_id):
+    result = db.restaurants.find_one({
+        'id':restaurant_id,
         'photos.filename':photo_id
     })
 
     photo_filepath = result[photo_id]['filepath']
-    return app.send_static_file('./photos/'+photo_filepath)
+    return app.send_static_file('./photos/' + photo_filepath)
 
+# get list of n top restaurants
 @restaurant.route('/get_list')
 def get_list():
-    pass
+    limit = request.args.get('limit')
 
+    # default: return 10 restaurants
+    if limit is None:
+        limit = 10
+
+    results = []
+    for place in db.restaurants.find({}).limit(int(limit)):
+        place = create_restaurant_response(place)
+        results.append(place)
+
+    return jsonify(results)
+
+def create_restaurant_response(place):
+    response = {}
+    response['_id'] = str(place.get('_id'))
+    response['name'] = place.get('name','')
+    response['location'] = place.get('location','')
+    response['formatted_phone_number'] = place.get('formatted_phone_number', '')
+    response['website'] =  place.get('website', '')
+    response['yelp_photos'] = place.get('yelp_photos', [])
+    response['yelp_location'] = place.get('yelp_location', {})
+
+    return response
