f7086ce43937b0e8c700f29d673d82133ad2c0e3
user <root@server.com>
2018-02-23 03:06:12 +0000

    made it use ssl for https for flask

--- server/main.py
+++ server/main.py
@@ -4,6 +4,15 @@ from restaurants import restaurant
 from extensions import app
 from flask_cors import CORS
 
+
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+crt = '/etc/letsencrypt/live/cherrypicker.io/fullchain.pem'
+key = '/etc/letsencrypt/live/cherrypicker.io/privkey.pem'
+# context.load_cert_chain('yourserver.crt', 'yourserver.key')
+context.load_cert_chain(crt, key)
+
+
 CORS(app)
 app.register_blueprint(restaurant)
 
@@ -20,6 +29,6 @@ def mainPage():
 if __name__ == "__main__":
     print("starting the server app.run()");
     app.debug = True;
-    app.run(port=4000, host='0.0.0.0')
+    app.run(port=4000, host='0.0.0.0', ssl_context=context)
 
 
